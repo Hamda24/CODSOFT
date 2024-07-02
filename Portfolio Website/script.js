2db24575fa1ffe7cbd5cb0266917e2c6ffcce621
Hamda24 <110939252+Hamda24@users.noreply.github.com>
2024-07-03 02:41:36 +0300

    updated (Footer, Resume)

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -2,3 +2,15 @@ function toggleMenu(){
     document.getElementById("navigations").classList.toggle("active");
 }
 
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const scrollY = window.scrollY; // Get the current scroll position
+  
+    const stickyThreshold = 50; // Adjust this value to control when it becomes sticky
+  
+    if (scrollY > stickyThreshold) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
+    }
+  });
